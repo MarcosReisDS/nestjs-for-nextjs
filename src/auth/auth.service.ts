2966983f6462a3c28ebc5809be0bb72f9f944ab4
Marcos Reis <marcos.santos@estadao.com>
2026-08-17 20:03:13 -0300

    aula 232

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from "@nestjs/common";
+import { LoginDto } from "./dto/login.dto";
 
 @Injectable()
 export class AuthService {
-    login() {
-        return "Olá AuthService";
+    login(loginDto: LoginDto) {
+        return loginDto;
     }
  }
