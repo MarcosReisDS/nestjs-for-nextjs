65b4b15615c2d6b3eae3096330a8153f44749ffe
Marcos Reis <marcos.santos@estadao.com>
2026-08-19 23:13:52 -0300

    aula 239

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,21 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CustomParseIntPipe } from 'src/common/pipes/custom-parse-int-pipe.pipe';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+    constructor(
+        private readonly userService: UserService
+    ) { }
+
     @Get(':id')
     findOne(@Param('id', CustomParseIntPipe) id: number) {
         return `Olá, usuário ${id}!`;
+    }
+
+    @Post()
+    async create(@Body() dto: CreateUserDto) {
+        return await this.userService.create(dto);
     }
 }
