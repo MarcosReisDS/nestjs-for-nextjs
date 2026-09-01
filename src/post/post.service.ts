4f22370bf67f294d8b886030f0b0130757b2a9d7
Marcos Reis <marcos.santos@estadao.com>
2026-08-31 22:09:27 -0300

    aula 252

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,12 +1,15 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable, Logger } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Post } from './entities/post.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreatePostDto } from './dto/create-post.dto';
 import { User } from 'src/user/entities/user.entity';
+import { createSlugFromText } from 'src/common/utils/create-slug-from-text';
 
 @Injectable()
 export class PostService {
+  private readonly logger = new Logger(PostService.name);
+
   constructor(
     @InjectRepository(Post)
     private readonly postRepository: Repository<Post>
@@ -14,14 +17,20 @@ export class PostService {
 
   async create(dto: CreatePostDto, author: User) {
     const post = this.postRepository.create({
-      slug: 'sdsdsd-' + Math.random().toString(36).slice(2, 8),
-      title: dto.title,
-      excerpt: dto.excerpt,
-      content: dto.content,
+      slug: createSlugFromText(dto.title),
       author,
+      content: dto.content,
+      excerpt: dto.excerpt,
+      coverImageUrl: dto.coverImageUrl,
+      title: dto.title,
     })
 
-    const created = await this.postRepository.save(post);
+    const created = await this.postRepository.save(post).catch((e: unknown) => {
+      if (e instanceof Error) {
+        this.logger.error('Erro ao criar post', e.stack);
+      }
+      throw new BadRequestException('Erro ao criar o post');
+    })
 
     return created;
   }
